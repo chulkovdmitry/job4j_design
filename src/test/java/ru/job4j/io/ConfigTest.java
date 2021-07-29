41e3c3486be38e741381dc19c83eb80c5d4ceeda
chulkovdmitry <dmitry_c2000@mail.ru>
2021-07-29 15:45:16 +0300

    1. Читаем файл конфигурации [#858]

---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.io;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNull;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"), is("Petr Arsentev"));
+    }
+
+    @Test
+    public void whenPairWithComment() {
+        String path = "./data/pair_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"), is("Petr Arsentev"));
+    }
+
+    @Test
+    public void whenPairEmpty() {
+        String path = "./data/pair_with_null.properties";
+        Config config = new Config(path);
+        config.load();
+        assertNull(config.value("name"));
+    }
+}
