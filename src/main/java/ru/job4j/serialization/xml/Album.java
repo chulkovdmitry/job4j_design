8688077bb5781bfe9e9619ad712ae46c5c936c98
chulkovdmitry <dmitry_c2000@mail.ru>
2021-08-17 16:37:11 +0300

    4. JAXB. Преобразование XML в POJO. [#315063]

--- src/main/java/ru/job4j/serialization/xml/Album.java
+++ src/main/java/ru/job4j/serialization/xml/Album.java
@@ -1,9 +1,19 @@
 package ru.job4j.serialization.xml;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "album")
 public class Album {
+        @XmlAttribute
         private int numberOfTracks;
+        @XmlAttribute
         private String name;
+        @XmlAttribute
         private int minutes;
+
+        public Album() {
+        }
 
         public Album(int numberOfTracks, String name, int minutes) {
             this.numberOfTracks = numberOfTracks;
--- src/main/java/ru/job4j/serialization/xml/Group.java
+++ src/main/java/ru/job4j/serialization/xml/Group.java
@@ -1,13 +1,29 @@
 package ru.job4j.serialization.xml;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.*;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Arrays;
 
+@XmlRootElement(name = "group")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Group {
+    @XmlAttribute
     private String name;
     private Album album;
+    @XmlAttribute
     private boolean active;
+    @XmlAttribute
     private int yearOfCreation;
+    @XmlElementWrapper()
+    @XmlElement(name = "person")
     private String[] crew;
+
+    public Group() {
+    }
 
     public Group(String name, Album album,
                boolean active, int yearOfCreation, String[] crew) {
@@ -16,26 +32,6 @@ public class Group {
         this.active = active;
         this.yearOfCreation = yearOfCreation;
         this.crew = crew;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Album getAlbum() {
-        return album;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public int getYearOfCreation() {
-        return yearOfCreation;
-    }
-
-    public String[] getCrew() {
-        return crew;
     }
 
     @Override
@@ -49,7 +45,7 @@ public class Group {
                 + '}';
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         final Group groupInfo = new Group(
                 "Fury",
                 new Album(14, "V", 82),
@@ -57,5 +53,25 @@ public class Group {
                 1983,
                 new String[]{"Jim", "Fred", "Jack"}
         );
+        // Получаем контекст для доступа к АПИ
+        JAXBContext context = JAXBContext.newInstance(Group.class);
+        // Создаем сериализатор
+        Marshaller marshaller = context.createMarshaller();
+        // Указываем, что нам нужно форматирование
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml = "";
+        try (StringWriter writer = new StringWriter()) {
+            // Сериализуем
+            marshaller.marshal(groupInfo, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        }
+        // Для десериализации нам нужно создать десериализатор
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            // десериализуем
+            Group groupInfo2 = (Group) unmarshaller.unmarshal(reader);
+            System.out.println(groupInfo2);
+        }
     }
 }
